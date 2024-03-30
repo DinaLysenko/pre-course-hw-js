2e2c4aa202da4b4f9a0b8118a8cb42de7741aa61
DinaLysenko <dinafate@rambler.ru>
2024-03-30 22:49:11 +0500

    hw2

--- lesson-01/02.js
+++ lesson-01/02.js
@@ -1,7 +1,9 @@
 // code
 let number1 = Math.floor(Math.random() * 100)
-if(number1%2 ===0){
+if(number1 % 2 ===0){
     console.log(`${number1}:Это четное число`)
 }else {
     console.log(`${number1}:Это нечетное число`)
 }
+let number = 1;
+console.log()
