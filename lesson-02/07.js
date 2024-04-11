dcd7fef55aa90d6f5f4e95f8afdb9499fac2ee33
DinaLysenko <dinafate@rambler.ru>
2024-04-11 13:18:39 +0500

    lesson2 hw7

---
+++ lesson-02/07.js
@@ -0,0 +1,14 @@
+let passportMarried = {
+    name: "Petr",
+    surname: "Petrov",
+    address: {
+        country: "USA",
+        city: "Bobryisk"
+    }
+};
+let passportMarriedCopy={
+    ...passportMarried,
+    married: true
+}
+console.log(passportMarried)
+console.log(passportMarriedCopy)
